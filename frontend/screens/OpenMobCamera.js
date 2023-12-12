96c4c8083b2f33430d76684a67d744b53183d666
hk819 <80596819+hk819@users.noreply.github.com>
2023-12-13 04:15:50 +0500

    Frontend Completed

--- frontend/screens/OpenMobCamera.js
+++ frontend/screens/OpenMobCamera.js
@@ -1,20 +1,16 @@
-// OpenMobCamera.js
-
-import React, { useEffect, useState, useRef } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
-import { Camera } from "expo-camera";
-import { Ionicons } from "@expo/vector-icons";
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View } from "react-native";
+import { RNCamera } from "react-native-camera";
 
 export default function OpenMobCamera() {
   const [hasPermission, setHasPermission] = useState(null);
-  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [cameraType, setCameraType] = useState(RNCamera.Constants.Type.back);
   const [ratio, setRatio] = useState("3:2"); // default is 4:3
   const cameraRef = useRef(null);
 
   useEffect(() => {
     (async () => {
-      const { status: cameraStatus } =
-        await Camera.requestCameraPermissionsAsync();
+      const { status: cameraStatus } = await RNCamera.requestPermissionsAsync();
       setHasPermission(cameraStatus === "granted");
     })();
   }, []);
@@ -28,38 +24,20 @@ export default function OpenMobCamera() {
     })();
   }, [cameraRef]);
 
-  const switchCamera = () => {
-    setCameraType(
-      cameraType === Camera.Constants.Type.back
-        ? Camera.Constants.Type.front
-        : Camera.Constants.Type.back
-    );
-  };
-
   return (
     <View style={{ flex: 1 }}>
-      <Camera
-        style={{ flex: 1 }}
-        type={cameraType}
-        ratio={ratio}
-        ref={cameraRef}
-      >
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: "transparent",
-            flexDirection: "row",
-          }}
-        >
-          <TouchableOpacity
-            style={{ flex: 0.1, alignSelf: "flex-end", alignItems: "center" }}
-            onPress={switchCamera}
-          >
-            <Text>Switch Camera</Text>
-            <Ionicons name="camera-reverse" size={24} color="white" />
-          </TouchableOpacity>
-        </View>
-      </Camera>
+      {hasPermission === null ? (
+        <View />
+      ) : hasPermission === false ? (
+        <Text>No access to camera</Text>
+      ) : (
+        <RNCamera
+          ref={cameraRef}
+          style={{ flex: 1 }}
+          type={cameraType}
+          ratio={ratio}
+        />
+      )}
     </View>
   );
 }
--- frontend/screens/RecordedVideo.js
+++ frontend/screens/RecordedVideo.js
@@ -1,45 +1,177 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import Video from "react-native-video";
+import React, { useState, useRef, useEffect } from "react";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Text,
+  Dimensions,
+  Alert,
+} from "react-native";
+import { Video } from "expo-av";
+import Icon from "react-native-vector-icons/FontAwesome";
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const RecordedVideo = () => {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const [showAlertIcon, setShowAlertIcon] = useState(false);
+  const videoRef = useRef(null);
+  let alertInterval;
+  const alertMessageTimeoutRef = useRef(null);
+  const alertIconTimeoutRef = useRef(null);
+
+  const handlePress = (video) => {
+    setSelectedVideo(video);
+    setIsFullscreen(true);
+
+    if (video === require("../assets/Hammering.mp4")) {
+      alertMessageTimeoutRef.current = setTimeout(() => {
+        Alert.alert("⚠️ Alert", "Harmful Behaviour Detected!");
+      }, 1000);
+
+      alertIconTimeoutRef.current = setTimeout(() => {
+        setShowAlertIcon(true);
+      }, 1500);
+    } else {
+      setShowAlertIcon(false);
+    }
+  };
+
+  const handleFullscreenExit = () => {
+    setIsFullscreen(false);
+    videoRef.current.pauseAsync();
+    setShowAlertIcon(false);
+    if (alertMessageTimeoutRef.current) {
+      clearTimeout(alertMessageTimeoutRef.current);
+    }
+    if (alertIconTimeoutRef.current) {
+      clearTimeout(alertIconTimeoutRef.current);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (alertMessageTimeoutRef.current) {
+        clearTimeout(alertMessageTimeoutRef.current);
+      }
+      if (alertIconTimeoutRef.current) {
+        clearTimeout(alertIconTimeoutRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Video
-        source={require("../assets/Hammering.mp4")} // Replace with the path to your first video file
-        style={styles.video}
-        controls={true}
-        resizeMode="contain"
-      />
-      <Video
-        source={require("../assets/HullaHoop.mp4")} // Replace with the path to your second video file
-        style={styles.video}
-        controls={true}
-        resizeMode="contain"
-      />
-      <Video
-        source={require("../assets/Hammering1.mp4")} // Replace with the path to your first video file
-        style={styles.video}
-        controls={true}
-        resizeMode="contain"
-      />
-    </View>
+    <ScrollView
+      contentContainerStyle={[
+        styles.container,
+        isFullscreen && styles.fullscreenContainer,
+      ]}
+    >
+      {!isFullscreen && (
+        <>
+          <TouchableOpacity
+            onPress={() => handlePress(require("../assets/Hammering.mp4"))}
+          >
+            <Text style={styles.text}>Hammering</Text>
+            <Video
+              ref={videoRef}
+              source={require("../assets/Hammering.mp4")}
+              style={styles.video}
+              controls={selectedVideo === require("../assets/Hammering.mp4")}
+              resizeMode="contain"
+              useNativeControls
+              isLooping
+            />
+            {showAlertIcon && (
+              <Icon
+                name="exclamation-triangle"
+                size={60}
+                color="#FF0000"
+                style={styles.alertIcon}
+              />
+            )}
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => handlePress(require("../assets/HullaHoop.mp4"))}
+          >
+            <Text style={styles.text}>Hulla Hoop</Text>
+            <Video
+              ref={videoRef}
+              source={require("../assets/HullaHoop.mp4")}
+              style={styles.video}
+              controls={selectedVideo === require("../assets/HullaHoop.mp4")}
+              resizeMode="contain"
+              useNativeControls
+              isLooping
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => handlePress(require("../assets/Hammering1.mp4"))}
+          >
+            <Text style={styles.text}>Hammering 1</Text>
+            <Video
+              ref={videoRef}
+              source={require("../assets/Hammering1.mp4")}
+              style={styles.video}
+              controls={selectedVideo === require("../assets/Hammering1.mp4")}
+              resizeMode="contain"
+              useNativeControls
+              isLooping
+            />
+          </TouchableOpacity>
+        </>
+      )}
+
+      {isFullscreen && (
+        <TouchableOpacity onPress={handleFullscreenExit}>
+          <Video
+            ref={videoRef}
+            source={selectedVideo}
+            style={styles.videoFullscreen}
+            resizeMode="contain"
+            shouldPlay
+            useNativeControls={false}
+          />
+        </TouchableOpacity>
+      )}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flexGrow: 1,
+    flexDirection: "column",
+    backgroundColor: "#fff", // Set background color to white
+  },
+  fullscreenContainer: {
     flex: 1,
     justifyContent: "center",
-    backgroundColor: "#000",
+    alignItems: "center",
   },
   video: {
+    width: "100%",
+    aspectRatio: 16 / 9,
+    marginBottom: 16,
+  },
+  videoFullscreen: {
+    width: windowWidth,
+    height: windowHeight,
+  },
+  text: {
+    color: "#000", // Set text color to black
+    fontSize: 18,
+    textAlign: "center",
+    marginVertical: 10,
+  },
+  alertIcon: {
     position: "absolute",
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
+    top: "50%",
+    left: "50%",
+    transform: [{ translateX: -15 }, { translateY: -15 }], // Adjust these values to center the icon
   },
 });
 
